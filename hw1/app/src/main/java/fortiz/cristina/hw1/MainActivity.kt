200247ead0f9d2cddc3d575736f562a5b53941ce
sneakyprincess <cfortiz1@umbc.edu>
2023-01-31 23:30:01 +0100

    saving

---
+++ hw1/app/src/main/java/fortiz/cristina/hw1/MainActivity.kt
@@ -0,0 +1,40 @@
+package fortiz.cristina.hw1
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import fortiz.cristina.hw1.ui.theme.HW1Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            HW1Theme {
+                // A surface container using the 'background' color from the theme
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    HW1Theme {
+        Greeting("Android")
+    }
+}
